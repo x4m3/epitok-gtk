3725d019bb88c83dde85bc2eb38c56ad51fe06c9
Philippe Loctaux <p@philippeloctaux.com>
2020-07-07 18:15:01 +0200

    created a blank window

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,27 @@
+use gio::prelude::*;
+use gtk::prelude::*;
+
 const PROGRAM_NAME: &str = "epitok";
+const APPLICATION_ID: &str = "com.philippeloctaux.epitok";
+
+fn build_ui(app: &gtk::Application) {
+    let application_window = gtk::ApplicationWindow::new(app);
+
+    application_window.set_title(PROGRAM_NAME);
+    application_window.set_border_width(10);
+    application_window.set_position(gtk::WindowPosition::Center);
+    application_window.set_default_size(1024, 600);
+
+    application_window.show_all();
+}
 
 fn main() {
-    glib::set_program_name(PROGRAM_NAME.into());
-    glib::set_application_name(PROGRAM_NAME);
+    let application = gtk::Application::new(Some(APPLICATION_ID), Default::default())
+        .expect("Failed to initialize gtk application");
+
+    application.connect_activate(|app| {
+        build_ui(app);
+    });
+
+    application.run(&std::env::args().collect::<Vec<_>>());
 }
